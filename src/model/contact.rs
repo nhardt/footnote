7abc148667332cb21faf856ac2d8863c0893998d
Nate Hardt <code@natehardt.dev>
2025-12-27 09:57:56 -0800

    contact: add PartialEq trait

--- src/model/contact.rs
+++ src/model/contact.rs
@@ -7,7 +7,7 @@ use serde::{Deserialize, Serialize};
 use std::fs;
 use std::path::Path;
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub struct Contact {
     #[serde(skip_serializing_if = "String::is_empty", default)]
     pub nickname: String,
