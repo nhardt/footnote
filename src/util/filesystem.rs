412ed9177b9f49d16a6a81d5d3b70452132b22e6
Nate Hardt <code@natehardt.dev>
2025-12-27 15:57:27 -0800

    mvp: make home.md if not exits

--- src/util/filesystem.rs
+++ src/util/filesystem.rs
@@ -1,4 +1,4 @@
-use crate::model::vault::Vault;
+use crate::model::{note::Note, vault::Vault};
 use anyhow::Result;
 use gethostname::gethostname;
 use std::{fs, path::PathBuf};
@@ -12,6 +12,11 @@ pub fn ensure_default_vault() -> Result<PathBuf> {
         fs::create_dir_all(&default_vault_dir)?;
         let hostname = gethostname();
         let _vault = Vault::create_primary(&default_vault_dir, "", &hostname.to_string_lossy())?;
+    }
+
+    let home_note_file = default_vault_dir.join("home.md");
+    if !home_note_file.exists() {
+        let _home_note = Note::create(&home_note_file, "Welcome to footnote.wiki");
     }
 
     Ok(default_vault_dir)
