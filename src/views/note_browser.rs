4e2d0d36d19b53c93341f0bc1d20cb2a76c0310c
Nate Hardt <code@natehardt.dev>
2025-12-27 21:41:58 -0800

    note: save!

--- src/views/note_browser.rs
+++ src/views/note_browser.rs
@@ -1,8 +1,29 @@
+use crate::{components::directoy_browser::DirectoryBrowser, context::VaultContext, Route};
 use dioxus::prelude::*;
+use footnote::platform::get_app_dir;
+use std::path::PathBuf;
 
 #[component]
 pub fn NoteBrowser() -> Element {
+    let vault_ctx = use_context::<VaultContext>();
+    let vault_path = match vault_ctx.get_vault() {
+        Some(path) => path.clone(),
+        None => return rsx! { "Error loading vault" },
+    };
+
+    let nav = navigator();
+
     rsx! {
-       "Note Browser",
+        DirectoryBrowser {
+            base_path: vault_path,
+            only_directories: false,
+            on_select: move |file_path:PathBuf| {
+                let path = file_path.display().to_string().clone();
+                let encoded = urlencoding::encode(&path);
+                nav.push(Route::NoteView { file_path: encoded.into_owned() });
+            },
+            on_cancel: move|_| info!("on_cancel"),
+            action_label: "Select Note".to_string(),
+        }
     }
 }
--- src/views/note_view.rs
+++ src/views/note_view.rs
@@ -1,9 +1,56 @@
 use dioxus::prelude::*;
-use dioxus_clipboard::prelude::use_clipboard;
+use footnote::model::note::Note;
+use std::path::PathBuf;
 
 #[component]
 pub fn NoteView(file_path: String) -> Element {
+    let decoded = urlencoding::decode(&file_path).unwrap();
+    let path = PathBuf::from(decoded.to_string());
+    let note = match Note::from_path(path.clone()) {
+        Ok(n) => n,
+        Err(_) => return rsx! { "Could not load note" },
+    };
+
+    // Signals for each field
+    //let mut title = use_signal(|| note.title.clone());
+    let mut body = use_signal(|| note.content.clone());
+    let mut share_with = use_signal(|| String::new());
+
+    let save_note = move |_| {
+        let mut updated_note = note.clone();
+        //updated_note.title = title.read().clone();
+        updated_note.content = body.read().clone();
+        updated_note.save(&path).unwrap();
+    };
+
     rsx! {
-       "Note View",
+        div { class: "flex flex-col h-full max-w-2xl",
+            h1 { class:"text-3xl font-bold", "{decoded}" }
+            div { class: "grid grid-cols-2",
+                label { "Title" }
+                input {
+                    class: "border-2",
+                    r#type: "text",
+                    value: "{decoded}",
+                }
+                label { "Shared with:" }
+                input {
+                    class: "border-2",
+                    r#type: "text",
+                    value: "{share_with}",
+                    oninput: move |e| share_with.set(e.value())
+                }
+            }
+            textarea {
+                class: "flex-1 w-full border-2",
+                value: "{body}",
+                oninput: move |e| body.set(e.value())
+            }
+            button {
+                class: "border-2 p-4 my-4",
+                onclick: save_note,
+                "Save"
+            }
+        }
     }
 }
