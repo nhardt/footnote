4c418375b3454ea7e8517c3ccbe48028a455d6bc
Nate Hardt <code@natehardt.dev>
2025-12-31 14:38:54 -0800

    note_browser: memoize vault path to avoid re-renders

--- src/views/note_browser.rs
+++ src/views/note_browser.rs
@@ -1,5 +1,5 @@
 use crate::{components::directoy_browser::DirectoryBrowser, context::VaultContext, Route};
-use dioxus::prelude::*;
+use dioxus::{logger, prelude::*};
 use footnote::{
     model::{note::Note, vault::Vault},
     platform::get_app_dir,
@@ -8,19 +8,15 @@ use std::{fmt::format, path::PathBuf};
 
 #[component]
 pub fn NoteBrowser() -> Element {
+    tracing::trace!("NoteBrowser re-render");
     let vault_ctx = use_context::<VaultContext>();
-    let vault_path = match vault_ctx.get_vault() {
-        Some(path) => path.clone(),
-        None => return rsx! { "Error loading vault" },
-    };
-    let vault = Vault::new(&vault_path)?;
-    let nav = navigator();
+    let vault_path = use_memo(move || vault_ctx.get_vault().unwrap().clone());
 
-    let mut err_message = use_signal(|| String::new());
+    let nav = navigator();
 
     rsx! {
         DirectoryBrowser {
-            base_path: vault_path,
+            base_path: vault_path(),
             only_directories: false,
             on_select: move |file_path:PathBuf| {
                 let path = file_path.display().to_string().clone();
@@ -28,13 +24,14 @@ pub fn NoteBrowser() -> Element {
                 nav.push(Route::NoteView { file_path: encoded.into_owned() });
             },
             on_file_create: move |file_path:PathBuf| {
+                let vault_path = vault_ctx.get_vault().expect("vault path should be valid");
+                let vault = Vault::new(&vault_path).expect("should be able to create vault from vault path");
                 if let Err(e) = vault.note_create(&file_path, "New Note") {
-                    err_message.set(format!("{}", e));
+                    eprintln!("error creating note: {}", e);
                 };
             },
             on_cancel: move|_| info!("on_cancel"),
             action_label: "Select Note".to_string(),
         }
-        label { "{err_message}" }
     }
 }
