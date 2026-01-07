91c790ccb451936a3421f9a12a4746f6a2edf96b
Nate Hardt <code@natehardt.dev>
2026-01-07 12:47:10 -0800

    ux: allow custom path and vault via ui
    this will need to get into the ui someday, for today i just need the
    ability to run two vaults on the same machine

--- src/util/filesystem.rs
+++ src/util/filesystem.rs
@@ -1,16 +1,32 @@
 use crate::model::{note::Note, vault::Vault, vault::VaultState};
 use anyhow::Result;
 use gethostname::gethostname;
-use std::{fs, path::PathBuf};
+use std::{env, fs, path::PathBuf};
 
 /// mvp: ensure the user has a footnote.wiki in their home directory
+/// mvp+1: allow custom via env var
 pub fn ensure_default_vault() -> Result<PathBuf> {
     // this doesn't yet "hang together". it would probably be clean to always
     // create a Vault standalone, then state transition to another state. the
     // current task is just to expose joining a vault from a secondary vault,
     // will revisit this later.
-    let app_dir = crate::platform::get_app_dir()?;
-    let default_vault_dir = app_dir.join("footnote.wiki");
+
+    let path_key = "PATH";
+    let vault_name_key = "VAULT";
+
+    let vault_path = match env::var(path_key) {
+        Ok(ref val) if val.is_empty() => crate::platform::get_app_dir()?,
+        Ok(val) => PathBuf::from(val),
+        Err(_) => crate::platform::get_app_dir()?,
+    };
+
+    let vault_name = match env::var(vault_name_key) {
+        Ok(ref val) if val.is_empty() => "footnote.wiki".to_string(),
+        Ok(val) => val,
+        Err(_) => "footnote.wiki".to_string(),
+    };
+
+    let default_vault_dir = vault_path.join(vault_name);
     fs::create_dir_all(&default_vault_dir)?;
     let vault = Vault::new(&default_vault_dir)?;
     match vault.state_read()? {
