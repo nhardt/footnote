00c2a9ac52354b3b77b78c06e40a3178903d0a13
Nate Hardt <nate.hardt@pm.me>
2026-01-21 12:07:29 -0800

    checkpoint: found app delegate and learned that openURL is already implemented

--- src/platform/ios.rs
+++ src/platform/ios.rs
@@ -1,11 +1,18 @@
+use objc2::ffi::*;
 use objc2::rc::Retained;
+use objc2::runtime::ProtocolObject;
+use objc2::runtime::{AnyClass, Bool, Sel};
+use objc2::runtime::{AnyObject, NSObject};
+use objc2::{msg_send, sel};
 use objc2::{MainThreadMarker, MainThreadOnly};
+use objc2_foundation::{NSArray, NSDictionary, NSString, NSURL};
+use objc2_ui_kit::UIApplicationDelegate;
+use objc2_ui_kit::{UIActivityViewController, UIApplication, UIViewController};
+use std::ffi::c_void;
+use std::ffi::CStr;
 use std::path::Path;
 use std::path::PathBuf;
-// Use AnyObject to satisfy the 'id' type requirement
-use objc2::runtime::{AnyObject, NSObject};
-use objc2_foundation::{NSArray, NSString, NSURL};
-use objc2_ui_kit::{UIActivityViewController, UIApplication, UIViewController};
+use std::ptr;
 
 pub const SHARE_SHEET_SUPPORTED: bool = true;
 
@@ -56,4 +63,60 @@ pub fn share_contact_file(file_path: &std::path::Path) -> Result<(), Box<dyn std
         root_vc.presentViewController_animated_completion(&controller, true, None);
     }
     Ok(())
+}
+
+pub fn inject_open_url_handler() {
+    let mtm = MainThreadMarker::new().expect("Must be on main thread");
+    let app = UIApplication::sharedApplication(mtm);
+
+    if let Some(delegate) = unsafe { app.delegate() } {
+        // Cast to AnyObject to get access to .class()
+        let obj = unsafe { &*(Retained::as_ptr(&delegate) as *const AnyObject) };
+        let cls = obj.class();
+        let sel = sel!(application:openURL:options:);
+        let cls_ref: &AnyClass = obj.class();
+        let cls_ptr = cls_ref as *const AnyClass as *mut AnyClass;
+
+        if cls.responds_to(sel) {
+            tracing::warn!("Delegate already implements openURL.");
+        } else {
+            unsafe {
+                // Obj-C Type Encodings:
+                // B is Bool, @ is object, : is selector
+                // The signature is: Bool (self, _cmd, app, url, options)
+                let types = CStr::from_bytes_with_nul(b"B@:@@@\0").unwrap();
+
+                // cast a Rust extern "C" fn to Obj-C Imp
+                let imp: objc2::runtime::Imp = std::mem::transmute(
+                    open_url_callback as unsafe extern "C" fn(_, _, _, _, _) -> _,
+                );
+
+                let success = objc2::ffi::class_addMethod(cls_ptr, sel, imp, types.as_ptr());
+
+                if success.as_bool() {
+                    tracing::info!(
+                        "Successfully injected application:openURL:options: into {:?}",
+                        cls.name() // Use {:?} because cls.name() returns &CStr
+                    );
+                } else {
+                    tracing::error!("Failed to inject method.");
+                }
+            }
+        }
+    }
+}
+
+extern "C" fn open_url_callback(
+    this: &AnyObject,
+    _sel: Sel,
+    _app: *mut AnyObject,
+    url: &NSURL,
+    _options: &NSDictionary<NSString, AnyObject>,
+) -> Bool {
+    let path = url.path();
+    tracing::info!("Successfully intercepted file open! Path: {:?}", path);
+
+    // TODO: Send this path to your Dioxus state via a channel or global
+
+    Bool::YES
 }
