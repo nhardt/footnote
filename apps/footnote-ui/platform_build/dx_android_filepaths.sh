c2d8279fd5700ad3cf9cabcfdfebe9ab23bc0efa
Nate Hardt <code@natehardt.dev>
2026-02-04 11:02:54 -0800

    footnote-ui -> footnote
    many things take the app name from the project name, rather than
    fight this just going to go with it

--- apps/footnote-ui/platform_build/dx_android_filepaths.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# until https://github.com/DioxusLabs/dioxus/pull/5195 or similar merges, insert this file into the build
-mkdir -p ../../target/dx/footnote-ui/release/android/app/app/src/main/res/xml
-cp ./platform_build/file_paths.xml ../../target/dx/footnote-ui/release/android/app/app/src/main/res/xml/
-mkdir -p ../../target/dx/footnote-ui/debug/android/app/app/src/main/res/xml
-cp ./platform_build/file_paths.xml ../../target/dx/footnote-ui/debug/android/app/app/src/main/res/xml/
