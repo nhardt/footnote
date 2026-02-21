f3da4b859b25e286e76490a06f78de7391e64b90
Nate Hardt <code@natehardt.dev>
2026-02-21 15:28:07 -0800

    devices: slightly improve active display
    this is overall a bit flaky still

--- apps/footnote/src/app/body/profile.rs
+++ apps/footnote/src/app/body/profile.rs
@@ -252,11 +252,10 @@ fn DeviceRow(device: Device, read_only: bool) -> Element {
                     if let Some(status) = outbound_status {
                         if let Some(current) = status.current {
                             div { class: "text-xs text-zinc-400",
-                                "*Active*",
+                                "Active",
                                 if let Some(total) = current.files_total {
-                                    "{current.files_transferred}/{total}"
+                                    "{current.files_transferred}/{total} files"
                                 }
-                                " files"
                             }
                         }
                         if let Some(success) = status.last_success {
@@ -275,15 +274,13 @@ fn DeviceRow(device: Device, read_only: bool) -> Element {
                         }
                     }
 
-
                     if let Some(status) = inbound_status {
                         if let Some(current) = status.current {
                             div { class: "text-xs text-zinc-400",
-                                "*Active*",
+                                "Active",
                                 if let Some(total) = current.files_total {
-                                    "{current.files_transferred}/{total}"
+                                    "{current.files_transferred}/{total} files"
                                 }
-                                " files"
                             }
                         }
                         if let Some(success) = status.last_success {
