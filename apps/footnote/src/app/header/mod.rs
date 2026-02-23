cc4f61e786330d8ef3cec702389884aba77b7bdd
Nate Hardt <nate.hardt@pm.me>
2026-02-23 13:28:50 -0800

    menu: hide sync button when irrelevant

--- apps/footnote/src/app/header/mod.rs
+++ apps/footnote/src/app/header/mod.rs
@@ -4,12 +4,19 @@ pub mod file_search;
 pub mod menu;
 pub mod sync_service_toggle;
 
+use footnote_core::model::vault::VaultState;
+
+use crate::context::AppContext;
+
 use file_search::FileSearch;
 use menu::HeaderMenu;
 use sync_service_toggle::SyncServiceToggle;
 
 #[component]
 pub fn Header(children: Element) -> Element {
+    let app_context = use_context::<AppContext>();
+    let vault_state = app_context.vault_state;
+
     rsx! {
         header {
             class: "sticky top-0 z-10 border-b border-zinc-800 bg-zinc-900/95 backdrop-blur-sm",
@@ -18,7 +25,13 @@ pub fn Header(children: Element) -> Element {
 
                 HeaderMenu {}
                 FileSearch {  }
-                SyncServiceToggle {}
+
+                match *vault_state.read() {
+                    VaultState::Primary | VaultState::SecondaryJoined => rsx! {
+                        SyncServiceToggle {}
+                    },
+                    _ => rsx! {}
+                }
             }
         }
     }
